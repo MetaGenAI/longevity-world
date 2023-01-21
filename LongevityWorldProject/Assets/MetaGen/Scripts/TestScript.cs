9cc86801f98a7d7fa92a837773d528920c11f2db
conath <chris@conath.me>
2023-01-21 23:50:09 +0100

    Add MetaGen folder with MIT License and Test Scene

---
+++ LongevityWorldProject/Assets/MetaGen/Scripts/TestScript.cs
@@ -0,0 +1,12 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TestScript : UdonSharpBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Hello World from UdonSharp!");
+    }
+}
