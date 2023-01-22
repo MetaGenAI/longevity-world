e58b7d2f45dfa6fbfdedc8c3344b94547d56a278
conath <chris@conath.me>
2023-01-22 01:05:39 +0100

    Add interactive button to test scene
    Move scene up to MetaGen folder
    Add basic button script

---
+++ LongevityWorldProject/Assets/MetaGen/Scripts/MyButton.cs
@@ -0,0 +1,15 @@
+﻿using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyButton : UdonSharpBehaviour
+{
+    public Text Text;
+    public GameObject WallToDisable;
+    
+    public void OnClick()
+    {
+        Text.text = "yee";
+        WallToDisable.SetActive(false);
+    }
+}
